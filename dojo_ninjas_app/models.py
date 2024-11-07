993a28e91dacc1cb531a7ba889cfcb6fbf1a115a
Anas Zughayyar <anas.ezzughayyar@gmail.com>
2024-11-07 14:40:21 +0200

    Update

--- dojo_ninjas_app/models.py
+++ dojo_ninjas_app/models.py
@@ -5,6 +5,7 @@ class Dojo(models.Model):
     name = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     state = models.CharField(max_length=2)
+    desc = models.TextField(default="old dojo")
 
 class Ninja(models.Model):
     dojo_id = models.ForeignKey(Dojo, related_name="ninjas" , on_delete=models.CASCADE)
